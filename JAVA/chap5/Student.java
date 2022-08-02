69ccf4a7b0c22e0b9f7a490dda321c0b37dfedf2
jisuMin <108313382+jisuMin@users.noreply.github.com>
2022-08-02 09:33:58 +0900

    Delete JAVA directory

--- JAVA/chap5/Student.java
+++
@@ -1,8 +0,0 @@
-package chap5;
-
-public interface Student {
-	/*public final static 자동정의*/ String name="학생";
-	/*모든 메소드 앞 abstract public 자동정의*/
-	void study();
-	void lunch();
-}
--- JAVA/chap5/Worker.java
+++
@@ -1,8 +0,0 @@
-package chap5;
-
-public interface Worker {
-	/*모든 메소드 앞 abstract public 자동정의*/
-	String name="교직원";
-	void work();
-	void lunch();
-}
